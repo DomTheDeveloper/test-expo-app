6cf5263d5a1526ab80e31b480f984c525612e6ab
DomTheDeveloper <DomTheDeveloper@users.noreply.github.com>
2026-07-15 10:06:15 -0700

    test: inspect Lean APIs for WOWII 143

---
+++ WOWII/Check143.lean
@@ -0,0 +1,28 @@
+import FormalConjectures.WrittenOnTheWallII.GraphConjecture143
+
+open Classical SimpleGraph
+
+#check List.getD_cons_zero
+#check List.getD_cons_succ
+#check List.Sorted.rel_get_of_lt
+#check List.Sorted.rel_get
+#check List.get?_eq_getElem?
+#check List.getD_eq_getElem?_getD
+#check List.getD_eq_getElem_getElem?_getD
+#check List.Perm.count_eq
+#check List.perm_sort
+#check List.sorted_sort
+#check Multiset.count_map
+#check Finset.one_lt_card
+#check Finset.one_lt_card_iff
+#check Finset.exists_ne_map_eq_of_one_lt_card
+#check SimpleGraph.countDegreeK
+#check SimpleGraph.degreeSequence
+#check SimpleGraph.Preconnected.degree_pos_of_nontrivial
+#check Nat.sSup_mem
+#check Finset.max'_mem
+#check Finset.le_max'
+#check SimpleGraph.Walk.connected_induce_support
+#check SimpleGraph.exists_walk_of_dist_ne_zero
+#check SimpleGraph.Walk.isPath_of_length_eq_dist
+#check SimpleGraph.Walk.length_eq_dist_of_subwalk
