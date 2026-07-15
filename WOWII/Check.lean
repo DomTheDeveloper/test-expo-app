abd41be8cca2f371109b99eeaa81ebee9e2aeb3a
DomTheDeveloper <DomTheDeveloper@users.noreply.github.com>
2026-07-15 08:35:03 -0700

    test: refresh WOWII Lean API checks

--- WOWII/Check.lean
+++ WOWII/Check.lean
@@ -5,13 +5,12 @@ open Classical SimpleGraph
 #check csSup_le
 #check csSup_le'
 #check sSup_le
-#check SimpleGraph.Connected.exists_spanningTree
-#check SimpleGraph.Connected.exists_spanning_tree
-#check SimpleGraph.exists_spanningTree
+#check SimpleGraph.Connected.exists_isTree_le
+#check SimpleGraph.toSubgraph
 #check SimpleGraph.Subgraph.IsSpanning
 #check Finset.card_filter_le
 #check Finset.card_le_univ
 #check Set.Nonempty.image
 #check Subgraph.verts
-#check Subgraph.IsSpanning.verts_eq
-#check Subgraph.IsSpanning.verts
+#check Subgraph.IsSpanning.verts_eq_univ
+#check Subgraph.spanningCoe
